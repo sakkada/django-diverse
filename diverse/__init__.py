57931897a8d1696918c3a76210aa8528c5f55dda
Guchetl Murat <gmurka@gmail.com>
2013-04-01 18:04:51 +0400

    update init minidoc (FS realted methods notice)

--- diverse/__init__.py
+++ diverse/__init__.py
@@ -1,14 +1,19 @@
 """
-methods on filesystem
-- size (_get_size)
-- chunks (reading by chunks)
-- multiple_chunks (size)
-- __iter__ (chunks)
-- __exit__ (close)
-- open
-- close
-image file
-- width
-- height
-- _get_image_dimensions
+django file object methods, which reads data from FS (be carefull, 
+calling one of them usually should be followed by closing file descriptor)
+
+file objects:
+    - size (_get_size)
+    - chunks (reading by chunks)
+    - multiple_chunks (size)
+    - __iter__ (chunks)
+    - __exit__ (close)
+    - open
+    - close
+image file objects:
+    - width
+    - height
+    - _get_image_dimensions
 """
+
+VERSION = (0, 1, 2)
