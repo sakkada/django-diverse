6cf8ae09ec5c9880bb8a5a1c6abcab9c3d1a3b35
Murat Guchetl <gmurka@gmail.com>
2017-11-18 00:02:55 +0300

    fixed setup.py error

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='.'.join(map(str, __import__('eavkit').VERSION)),
+    version='.'.join(map(str, __import__('diverse').VERSION)),
 
     description = ('Application for extended files management,'
                    ' based on builtins File/Image Django database'
